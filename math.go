a081494fd38d1433483572e54ee6dcd09d34ef4a
Timur Rakhmanov <rakhmanovtim@gmail.com>
2024-02-24 20:33:21 +0900

    Add Go Doc comments

--- math.go
+++ math.go
@@ -1,5 +1,10 @@
+// math package contains a simple function
+// for the addition of two numbers
 package math
 
+// Add adds two given integer numbers
+// and returns the resulting int.
+// Read further [here]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
